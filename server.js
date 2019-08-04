1dad65d9123ec1f0d8d456297943f7492909ff50
eermac949 <email.briandateling@gmail.com>
2019-08-04 22:21:36 +1000

    make it better

--- server.js
+++ server.js
@@ -1,34 +1,25 @@
-//In our packages.json file we use nodemon for live changes with devStart
-
-if(process.env.NODE_ENV !== 'production'){
-    require('dotenv').config();
-}
-
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-
-const indexRouter = require('./routes/index')// ./ relative to current location
-
-app.set('view engine', 'ejs')//Our display language
-app.set('views', __dirname +'/views')//changing the dir for the ejs files
-app.set('layout', 'layouts/layout')//every file that placed in layout 
-//file is to keep layouts for replication later
-app.use(expressLayouts)
-app.use(express.static('public'))
-
-const mongoose = require('mongoose')//import mongoose
-//Using.env need to use npm env dev thing
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true})//Mongoose uses old method from mongoDB
-    const db = mongoose.connection
-    db.on('error', error => console.error(error))
-    db.once('open', () => console.log('Connected to Mongoose'))
-    //never hardcode connection. 
-//Dependent on environment. Want to connect to 
-//{these are the options to setup mongoDB}
-
-app.use('/', indexRouter)
-
-app.listen(process.env.PORT || 3000)//Pulls from process environment on port..
-
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').load()
+  }
+  
+  const express = require('express')
+  const app = express()
+  const expressLayouts = require('express-ejs-layouts')
+  
+  const indexRouter = require('./routes/index')
+  
+  app.set('view engine', 'ejs')
+  app.set('views', __dirname + '/views')
+  app.set('layout', 'layouts/layout')
+  app.use(expressLayouts)
+  app.use(express.static('public'))
+  
+  const mongoose = require('mongoose')
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
+  
+  app.use('/', indexRouter)
+  
+  app.listen(process.env.PORT || 3000)
