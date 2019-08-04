6a9ba86b39b2b0f4d6f0003acc8229d0404144c0
eermac949 <email.briandateling@gmail.com>
2019-08-04 22:30:28 +1000

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req,res) => {
-    // res.send('Hello World - Kappa') Sends basic text
-    res.render('index')
+router.get('/', (req, res) => {
+  res.render('index')
 })
 
-module.exports = router//Need to export to Server
+module.exports = router
