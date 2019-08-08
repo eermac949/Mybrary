b05826a503086c4fa2f6beb9f364e7d6899ba3d4
eermac949 <email.briandateling@gmail.com>
2019-08-08 13:01:38 +1000

    finalise Book routes

--- routes/books.js
+++ routes/books.js
@@ -26,6 +26,7 @@ router.get('/', async (req, res) => {
         })
     } catch {
         res.redirect('/')
+        //console.log('error')
     }
 })
 
@@ -53,26 +54,112 @@ router.post('/', async (req, res) => {
         res.redirect(`books`)
     } catch {
         renderNewPage(res, book, true)//pass existing book and true for error
+        //console.log('error')
     }
 })
 
+//Show Book Route
+router.get('/:id', async (req,res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+                                .populate('author')
+                                .exec()
+//pupolate author variable with all the information(object info)
+        res.render('books/show', {book:book})
+    } catch  {
+        res.redirect('/')
+        //console.log('error')
+    }
+})
+
+//Edit book route
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        renderEditPage(res, book)
+    } catch {
+        //console.log('error')
+        res.redirect('/')
+    }
+  })
+
+  //Update Book Route
+  router.put('/:id', async (req, res) => {
+    //We set it to null so we can get an error when they haven't uploaded one
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        book.title = req.body.title
+        book.author = req.body.author
+        book.publishDate = new Date(req.body.publishDate)
+        book.pageCount = req.body.pageCount
+        book.description = req.body.description
+        if (req.body.cover != null && req.body.cover !== ''){
+            saveCover(book, req.body.cover)
+        }
+        await book.save()
+        res.redirect(`/books/${book.id}`)
+    } catch /*(err)*/ {
+        //console.log(err)//Detailed Error message code
+        if (book != null) {
+            renderEditPage(res, book, true)//pass existing book and true for error
+        } else {
+            //console.log('error')
+            redirect('/')
+        }
+        
+    }
+})
+
+router.delete('/:id', async (req,res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id)
+        await book.remove()
+        res.redirect('/books')
+    } catch {
+        if (book != null) {
+            res.render('books/show', {
+                book: book,
+                errorMessage: 'Could not remove book'
+            })
+        } else {
+            res.redirect('/')
+        }
+    }
+})
 
 //Default set hasError to false
 async function renderNewPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'new', hasError)
+}
+
+async function renderEditPage(res, book, hasError = false) {
+    renderFormPage(res, book, 'edit', hasError)
+}
+
+//For duplication
+async function renderFormPage(res, book, form, hasError = false) {
     try {
         const authors = await Author.find({})//get all authors
         const params = {
             authors: authors,
             book: book
         }
-        if (hasError) params.errorMessage = 'Error Creating Book'
-        res.render('books/new', params)
+        if (hasError) {
+            if (form == 'edit') {
+                params.errorMessage = 'Error Updating Book'
+            } else {
+                params.errorMessage = 'Error Creating Book'
+            }
+        }
+        res.render(`books/${form}`, params)
     } catch{
         res.redirect('/books')
     }
 }
 
-function savecover(book, coverEncoded){
+function saveCover(book, coverEncoded){
     if (coverEncoded == null) return 
     const cover = JSON.parse(coverEncoded)
     // check if there is a cover and its the correct file type
